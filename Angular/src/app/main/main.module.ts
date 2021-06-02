fc30f80864560b6eec6c9e02ab90123547e7f7b5
RAVI SHANKAR <katakamravishankar@gmail.com>
2021-06-03 00:08:26 +0530

    Added new components

--- Angular/src/app/main/main.module.ts
+++ Angular/src/app/main/main.module.ts
@@ -5,11 +5,14 @@ import { RouterModule } from '@angular/router';
 import { ContentResolver } from '../services/contentResolver';
 import { ServicesComponent } from './services/services.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TermsandcondComponent } from './others/termsandcond/termsandcond.component';
+import { PrivacypolicyComponent } from './others/privacypolicy/privacypolicy.component';
+import { AboutusComponent } from './others/aboutus/aboutus.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent, ServicesComponent],
+  declarations: [HomeComponent, ServicesComponent, TermsandcondComponent, PrivacypolicyComponent, AboutusComponent],
   imports: [
     RouterModule.forChild([
     {
@@ -20,6 +23,16 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     {
       path: 'services',
       component: ServicesComponent,
+      resolve: {content: ContentResolver}
+    },
+    {
+      path: 'termsandconditions',
+      component: TermsandcondComponent,
+      resolve: {content: ContentResolver}
+    },
+    {
+      path: 'privacypolicy',
+      component: PrivacypolicyComponent,
       resolve: {content: ContentResolver}
     }
     ]),
