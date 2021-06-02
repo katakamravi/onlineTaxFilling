dcba1b907de9ad86c2bfe5d5dc6870c592b78ec4
RAVI SHANKAR <katakamravishankar@gmail.com>
2021-06-02 16:22:31 +0530

    Added client id in email

--- Node/routes/users.js
+++ Node/routes/users.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var Razorpay = require('razorpay');
 var nodemailer = require('nodemailer');
 const { google } = require("googleapis");
-const { gmail } = require('googleapis/build/src/apis/gmail');
 const OAuth2 = google.auth.OAuth2;
 
 
@@ -33,93 +32,15 @@ router.post('/orderid', function (req, res, next) {
   });
 });
 
-// router.post('/email', function (req, res, next) {
-//   const response = userEmail(req);
-//   res.send(response);
-// });
-
 
 router.post('/email', function (req, res, next) {
-  //const clientId = '996320987644-6a1fdc8tbvftoudbuf3st7pt6dlfk1hu.apps.googleusercontent.com';
-  //const clientSecret = 'QSy3IhQblfYsZ-sukWBgm6Kf';
-  //const refresh = '1//0g7Jug2SA3r6vCgYIARAAGBASNwF-L9Ir2VDaj-6McDiI3GXmud73C-4eTUmh0BWpthA_uQUsXFLQyr0HG4WteAGP8I1SsuYHaPU';
-//   const oauth2Client = new OAuth2(
-//     clientId,
-//     clientSecret
-//   );
-//   oauth2Client.setCredentials({
-//     refresh_token: refresh
-// });
-// console.log(re);
-  // const newAccessToken = oauth2Client.getAccessToken();
-  // var transporter = nodemailer.createTransport({
-  //   service: 'Gmail',
-  //   auth: {
-  //     type: 'OAuth2',
-  //     user: 'katakamravishankar@gmail.com',
-  //     clientId: clientId,
-  //     clientSecret: clientSecret,
-  //     refreshToken: refresh,
-  //     accessToken: "ya29.a0AfH6SMC9vNOet-tVCob6Fn__kdg1oSvZQpHXgTHeIIag11n7ndGzj0IpzHGE0EhQb6jJ8meHNFX3YP1R7GZKhnoJrIpCfgTZ5CNRXfy8jx7Yw-wwd-i12wD8PHayxnhryaSyjyQQF2PFTlU2Xof_C42vA5yx"
-  //   }
-  // });
- 
-//   const oauth2Client = new OAuth2(
-//     clientId,
-//     clientSecret,
-// );
-
-// oauth2Client.setCredentials({
-//     refresh_token: refresh
-// });
-// const newAccessToken = oauth2Client.getAccessToken()
-// let transporter = nodemailer.createTransport(
-//     {
-//         service: 'Gmail',
-//         auth: {
-//             type: 'OAuth2',
-//             user: 'katakamravishankar@gmail.com',
-//             clientId: clientId,
-//             clientSecret: clientSecret,
-//             refreshToken: refresh,
-//             accessToken: newAccessToken
-//         }
-//     },
-//     {
-//         // default message fields
-
-//         // sender info
-//         from: 'RaviShankar Katakam <katakamravishankar@gmail.com>'
-//     }
-// );
-// var mailOptions = {
-//   from: 'katakamravishankar@gmail.com',
-//   to: req.body.userEmail,
-//   subject: `Contact name: ${req.body.userName}`,
-//   html: `<h1>Contact details</h1>
-// <h2> name:${req.body.userName} </h2><br>
-// <h2> email:${req.body.userEmail} </h2><br>
-// <h2> phonenumber:${req.body.userMobile} </h2><br>`
-// };
-// transporter.sendMail(mailOptions, function (error, response) {
-//   if (error) {
-//     console.log(error);
-//     next(error); // if error occurs send error as response to client
-//   }
-//   else {
-//     console.log('Email sent: ' +response);
-//     res.send({ message: 'Email Sent Successfully', value: response })//if mail is sent successfully send Sent successfully as response
-//   }
-// });
-
 const oauth2Client = new OAuth2(
-    " ", // ClientID
-    " ", // Client Secret
-    " " // Redirect URL
+    "172708659579-biku41tgjdifian2qn0eh7oki5mhb7ei.apps.googleusercontent.com", // ClientID
+    "D3OXfJ1K_ViHkvwO_F7PAi7e", // Client Secret
+    "https://developers.google.com/oauthplayground" // Redirect URL
 );
-
 oauth2Client.setCredentials({
-    refresh_token: " "
+    refresh_token: "1//04jt5I457D8jyCgYIARAAGAQSNwF-L9IrJsvwUIanxgvL5RKNeQ75S_vhKn-bvQRFUJg9tm6SQl7-VfebKG_hqUg15n3E68djc-E"
 });
 const accessToken = oauth2Client.getAccessToken();
 
@@ -127,10 +48,10 @@ const smtpTransport = nodemailer.createTransport({
     service: "gmail",
     auth: {
          type: "OAuth2",
-         user: "srinivasguptha.k@gmail.com", 
-         clientId: " ",
-         clientSecret: " ",
-         refreshToken: "",
+         user: "katakamravishankar@gmail.com", 
+         clientId: "172708659579-biku41tgjdifian2qn0eh7oki5mhb7ei.apps.googleusercontent.com",
+         clientSecret: "D3OXfJ1K_ViHkvwO_F7PAi7e",
+         refreshToken: "1//04jt5I457D8jyCgYIARAAGAQSNwF-L9IrJsvwUIanxgvL5RKNeQ75S_vhKn-bvQRFUJg9tm6SQl7-VfebKG_hqUg15n3E68djc-E",
          accessToken: accessToken
     }
 });
@@ -138,7 +59,7 @@ const smtpTransport = nodemailer.createTransport({
 const mailOptions = {
     from: "srinivasguptha.k@gmail.com",
     to: `${req.body.userEmail}`,
-    subject: "Testing Mail Id",
+    subject: "Testing Mail",
     generateTextFromHTML: true,
     html: `<h1>Contact details</h1>
     <h2> name:${req.body.userName} </h2><br>
@@ -148,7 +69,8 @@ const mailOptions = {
 };
 
 smtpTransport.sendMail(mailOptions, (error, response) => {
-    error ? console.log(error) : console.log(response);
+    if(error) next(err);
+    res.json({"success": true, Data: response})
     smtpTransport.close();
 });
 });
